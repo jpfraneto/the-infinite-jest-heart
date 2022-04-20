2f0de4e4a246ef1c88ce189e20009f93eeda44e4
jpfraneto <jpfraneto@gmail.com>
2022-04-20 11:18:46 -0400

    now

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const moment = require('moment');
 let express = require('express');
 let router = express.Router();
-const middlewareObj = require('../middleware');
 let Recommendation = require('../models/recommendation');
 let Cycle = require('../models/cycle');
 let theSource = require('../middleware/theSource');
